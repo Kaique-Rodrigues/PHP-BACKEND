68038a4a4ca12be48be9b315b215d5d0590e21e9
kaique <kaiquejuta3@hotmail.com>
2020-03-26 22:28:04 -0300

    Variaveis, Arrays comuns e Associativos, Operações e Condicionais

--- Introducao-php/index.php
+++ Introducao-php/index.php
@@ -72,3 +72,55 @@ echo $resultado4 = $numero1 / $numero2. "<br>"; // Divisão
 echo $resultado5 = $numero1 % $numero2. "<br>"; // Resto da Divisão
 echo  $resultado6 = $numero1 ** $numero2. "<br>"; // Potenciação
 ?>
+
+<hr>
+<?php
+     echo "<h1>Condicionais</h1>". "<br>";
+
+echo "<h3>IF e Else</h3><br>"; // A condição deve ser True ou Flase 
+
+    $diaEnsolarado = true;
+
+    if($diaEnsolarado == true){
+        echo "Bora pra praia !!!" . "<br>";
+    }else{
+        echo "Cuidado com a chuva :(". "<br>";
+    
+}
+    echo "<h3>Else if</h3>". "<br>";
+    $idade = 72;
+
+    if($idade < 16){
+        echo "Não pode votar". "<br>";
+    } else if ($idade == 16 && $idade == 17 || $idade > 70){
+        echo "Voto opcional" . "<br>";
+    }else if ($idade > 18 || $idade < 70){
+        echo "Voto Obrigatório". "<br>";
+    }
+
+    echo "<h3>If ternário</h3>". "<br>"; // O if ternário é um operador que retorna um valor
+
+    $numeroA = 32;
+    $numeroB = 58;
+
+    $Maior = $numeroA > $numeroB ? $numeroA : $numeroB;
+    echo $Maior. "<br>";
+
+
+    echo "<h3>Switch</h3>". "<br>";
+
+    $numero = 0;
+
+    switch ($numero){
+        case 1:
+        echo "O Número é igual a 1". "<br>";
+    break;
+    case 2:
+    echo "O Número é igual a 2". "<br>";
+break;
+    default:
+    echo "O Número é diferente de 1 e 2". "<br>";
+    break;
+    }
+
+?>
---
+++ lacos-de-repeticoes/home.php
@@ -0,0 +1,9 @@
+<?php
+    
+    $numero1 = 32;
+    $numero2 = 58;
+
+    $maior = $numero1 > $numero2 ? $numero1 : $numero2;
+
+    echo $maior;
+?>
